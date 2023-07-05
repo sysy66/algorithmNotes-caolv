da3462d8336876c77dc4f6684c1ea2a70a430171
caolv <shaoyinshaoyang@gmail.com>
2023-07-05 22:39:04 +0800

    2614第一题

---
+++ python/2614.对角线上的质数.py
@@ -0,0 +1,41 @@
+# url:"https://leetcode.cn/problems/prime-in-diagonal/solutions/2216309/python3zhi-shu-shai-bian-li-by-musing-cl-aqpy/"
+import time
+from typing import List
+
+
+MX_RANGE = 4 * 10 ** 6 + 1
+is_prime = [True] * MX_RANGE
+primes = list()
+for i in range(2, MX_RANGE):
+    if is_prime[i]: primes.append(i)
+    for p in primes:
+        if i * p >= MX_RANGE: break
+        is_prime[i * p] = False
+        if i % p == 0: break
+primes = set(primes)
+
+class Solution:
+    def diagonalPrime(self, nums: List[List[int]]) -> int:
+        ret, n = 0, len(nums)
+        for i in range(n):
+            x = nums[i][i]
+            if x in primes and x > ret: ret = x
+            x = nums[i][n - 1 - i]
+            if x in primes and x > ret: ret = x
+        return ret
+
+
+t_0 = time.time()
+
+s = Solution()
+func_name = dir(s)[-1]
+func = getattr(s, func_name)
+
+print(func(nums = [[1,2,3],[5,6,7],[9,10,11]]))
+# 输出：11
+print(func(nums = [[1,2,3],[5,17,7],[9,11,10]]))
+# 输出：17
+
+t_1 = time.time()
+print(t_1 - t_0)
+
