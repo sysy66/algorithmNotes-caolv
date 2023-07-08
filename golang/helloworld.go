6cd7ae888466cc9263453bfb2a90e29b4a255f7a
caolv <shaoyinshaoyang@gmail.com>
2023-07-08 08:07:03 +0800

    delete .idea on desktop

--- golang/helloworld.go
+++ golang/helloworld.go
@@ -6,6 +6,3 @@ func main() {
 	fmt.Println("HelloWorld")
 	fmt.Println("Go Go Go !!!")
 }
-
-# url = git@github.com:sysy66/algorithmNotes-caolv.git
-# url = https://github.com/sysy66/algorithmNotes-caolv.git
