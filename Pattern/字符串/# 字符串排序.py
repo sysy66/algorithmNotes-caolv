d94fec5e2b12bb54d83c928267f395ec1dcaa9df
sysy666 <84129680+sysy66@users.noreply.github.com>
2023-08-03 10:13:08 +0800

    update 字符串排序。。。

--- Pattern/字符串/# 字符串排序.py
+++ Pattern/字符串/# 字符串排序.py
@@ -1,5 +1,10 @@
 # 字符串排序.py
 
 s = 'bachfmsbw'
-s = "".join((lambda x:(x.sort(),x)[1])(list(s)))
-print(s)
+
+s1 = "".join((lambda x:(x.sort(), x)[1])(list(s)))
+print(s1)
+
+s2 = ''.join(sorted(s))
+print(s2)
+
